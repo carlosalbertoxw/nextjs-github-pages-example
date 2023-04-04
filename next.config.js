54eab9bc4c826c28c814a8c1a297fcaf28d42d53
Carlos Alberto <carlosalbertoxw@gmail.com>
2023-04-03 23:18:12 -0600

    Initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Use the prefix for GitHub Pages
+  basePath: '/nextjs-github-pages-example',
+  assetPrefix: './',
+  // Enable static export
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
 }
 
 module.exports = nextConfig
